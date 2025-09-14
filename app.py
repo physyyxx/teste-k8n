60bf6a4e55217151695a3e0721876e8f8b384a88
Bruno Peixoto <bruno961@gmail.com>
2025-09-14 11:57:52 -0300

    Alteracao teste

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Opa, mudou!!!"
+    return "<h1>Mais uma mudança</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
